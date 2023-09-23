133d80a2ee2964bb60e73731b12c88a1d5d4283e
Nezumi <1094270256@qq.com>
2023-09-23 18:13:09 +0800

    info

--- triangleArea.js
+++ triangleArea.js
@@ -1,5 +1,9 @@
 function triangleArea(a, b, c) {
-    p = (a + b + c) / 2
-    s = Math.sqrt(p * (p - a) * (p - b) * (p - c))
-    return s
+    if (a + b < c & a + c < b & b + c < a) {
+        p = (a + b + c) / 2
+        s = Math.sqrt(p * (p - a) * (p - b) * (p - c))
+        console.log(s)
+    }
+    else console.log("请输入正确的三角形三边长！")
+    return
 }
