8c85134d729e2d4fa945df1104fdae3f5f2bf4db
TaijinKyofushou <95996141+TaijinKyofushou@users.noreply.github.com>
2023-09-23 20:11:01 +0800

    Update printPrimesInRange.js

--- 第二周练习/printPrimesInRange.js
+++ 第二周练习/printPrimesInRange.js
@@ -1,7 +1,7 @@
 function isPrime(n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
-    if (n % 2 === 0 || n % 3 === 0) return false;
+    if (n % 2 === 0 || n % 3 === 0) return false; //"1" == 1, 1 === 1
     for (var i = 5; i * i <= n; i += 6) //除了2和3之外的素数都可以表示成6k±1的形式
     { 
         if (n % i === 0 || n % (i + 2) === 0) return false;
