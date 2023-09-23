e010b6b3f21463633d547b8c90de1338b18ce686
TaijinKyofushou <95996141+TaijinKyofushou@users.noreply.github.com>
2023-09-23 18:19:52 +0800

    triangleArea.js

--- triangleArea.js
+++ triangleArea.js
@@ -3,7 +3,6 @@ function triangleArea(a, b, c) {
         p = (a + b + c) / 2;
         s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return s;
-    } else {
-        return "请输入正确的三角形三边长！";
-    }
+    } 
+    else return "请输入正确的三角形三边长！";
 }
